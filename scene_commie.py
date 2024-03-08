56108f0e2a756707919768184d3b6a7a930825ff
acsvln <acsvln@disroot.org>
2024-03-08 04:37:14 +0300

    scene_commie.py fixed

--- scene_commie.py
+++ scene_commie.py
@@ -1,52 +1,51 @@
+from asciimatics.screen import Screen
 from asciimatics.scene import Scene
 from asciimatics.paths import Path
 from asciimatics.effects import Print, Effect, Sprite
 from asciimatics.renderers import FigletText, StaticRenderer
 
+from utils import *
 from resources import *
 
-class DrawLine(Effect):
-    def __init__(self, screen, x1, y1, x2, y2):
-        super().__init__(screen)
-        self.x1 = x1
-        self.x2 = x2
-        self.y1 = y1
-        self.y2 = y2
-        # self.screen = screen
-
-    def _update(self, frame_no):
-        self.screen.move(self.x1, self.y1)
-        self.screen.draw(self.x2, self.y2, '~', )
-
-    def reset(self):
-        pass
-
-    def stop_frame(self):
-        pass
 
 def scene_commie(screen, duration):
     path = Path()
     path.jump_to(37, 15)
     path.move_straight_to(37, screen.height, 40)
 
+    caption_x = screen.width - 86
+    caption_y = screen.height - 10
+
+    delta = 9
+
     effects = [
         Print(
             screen,
             FigletText('To communism...', font='big', width=400),
-            x=72,
-            y=56
+            x=caption_x,
+            y=caption_y,
+            colour=Screen.COLOUR_RED
         ),
         Print(
             screen,
             StaticRenderer([commie1]),
-            x=100,
-            y=30
+            x=10 + delta,
+            y=2 + delta,
+            colour=Screen.COLOUR_RED
         ),
         Print(
             screen,
             StaticRenderer([lenin]),
-            x=150,
-            y=30
+            x=screen.width - textBlockWidth(lenin) - 3 * delta,
+            y=4,
+            colour=Screen.COLOUR_YELLOW
+        ),
+        Print(
+            screen,
+            StaticRenderer([cat]),
+            x=screen.width - textBlockWidth(cat) - 8 * delta,
+            y= caption_y - textBlockHeight(cat) + 5 - delta,
+            colour=Screen.COLOUR_YELLOW
         ),
     ]
 
